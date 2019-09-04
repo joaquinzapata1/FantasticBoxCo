4cf7178bd4b4c8b0ced70dd14391c26fdb7652bc
joaquinzapata1 <38268789+joaquinzapata1@users.noreply.github.com>
2019-09-04 04:16:00 -0300

    Add files via upload
    Finished and deployed FantasticBoxCo

---
+++ precache-manifest.c8d5377a8bcfa515af5e803b7b87163b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338c00f7dc96621b716ca675a84f7911",
+    "url": "/FantasticBoxco/index.html"
+  },
+  {
+    "revision": "8fd788f6d16ff72783c7",
+    "url": "/FantasticBoxco/static/css/main.f4343dfc.chunk.css"
+  },
+  {
+    "revision": "3f1951a4320b4776758f",
+    "url": "/FantasticBoxco/static/js/2.c950d02b.chunk.js"
+  },
+  {
+    "revision": "8fd788f6d16ff72783c7",
+    "url": "/FantasticBoxco/static/js/main.de492c3d.chunk.js"
+  },
+  {
+    "revision": "aaad1b4e855f5468bdf8",
+    "url": "/FantasticBoxco/static/js/runtime~main.b70f41e0.js"
+  }
+]);
